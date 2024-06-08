fa1af06e57045eb2abd911fe751889df478fb317
Victor Fasano <49087202+fasanoraful@users.noreply.github.com>
2024-06-08 01:18:33 -0300

    Update lemp_install.sh

--- lemp_install.sh
+++ lemp_install.sh
@@ -42,6 +42,8 @@ if [ "$(whoami)" != 'root' ]; then
   exit 1
 fi
 
+apt-get install git unzip zip gdb screen htop build-essential pkg-config libboost-dev libgmp3-dev libxml2-dev sqlite3 libsqlite3-dev libtcmalloc-minimal4 liblua5.1 libmysqlclient-dev ccache libboost-filesystem-dev libboost-regex-dev libboost-system-dev libboost-thread-dev libboost-iostreams-dev
+
 if [ "$INSTALL_NGINX" != "True" ] && [ "$INSTALL_MYSQL" != "True" ] && [ "$INSTALL_PHP" != "True" ]; then
   echo "Please set some values to True for the script to run!"
   exit 1
