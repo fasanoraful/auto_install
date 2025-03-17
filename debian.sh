6cf7e0812577237c9c254dc3c781dfe5a450cd92
Victor Fasano <49087202+fasanoraful@users.noreply.github.com>
2025-03-16 22:36:48 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -10,13 +10,18 @@
 # 5) Let'sEncrypt SSL para o website
 ################################################################################
 
-# Função de exibição de banner
-function banner() {
-  echo "+-----------------------------------------------------------------------+"
-  printf "| %-65s |\n" "`date`"
-  echo "|                                                                       |"
-  printf "| %-65s |\n" "$1"
-  echo "+-----------------------------------------------------------------------+"
+# Função de exibição de barra de progresso
+function progress_bar() {
+  local progress=$1
+  local total=$2
+  local percentage=$((progress * 100 / total))
+  local bar_length=50
+  local filled_length=$((bar_length * percentage / 100))
+  local empty_length=$((bar_length - filled_length))
+  local bar=""
+  for ((i = 0; i < filled_length; i++)); do bar="${bar}#"; done
+  for ((i = 0; i < empty_length; i++)); do bar="${bar}."; done
+  echo -ne "\r[${bar}] ${percentage}%"
 }
 
 # Verificação se o script está sendo executado como root
@@ -42,23 +47,26 @@ INSTALL_PHPMYADMIN="True"
 ENABLE_SSL="False"
 
 # Início da instalação
-banner "Instalação automática do servidor LEMP iniciada. Isso pode levar alguns minutos..."
+echo "Instalação automática do servidor LEMP iniciada."
 
 # Atualização do sistema
 echo -e "\n---- Atualizando o sistema ----"
 apt update -y && apt upgrade -y
+progress_bar 1 5
 
 # Instalando pacotes essenciais
 echo -e "\n---- Instalando pacotes básicos ----"
 apt install -y curl wget gnupg2 ca-certificates lsb-release apt-transport-https unzip zip git htop
+progress_bar 2 5
 
 # Adicionando repositório do PHP para Debian
 echo -e "\n---- Adicionando repositório do PHP ----"
 wget -qO - https://packages.sury.org/php/apt.gpg | tee /etc/apt/trusted.gpg.d/php.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
 apt update -y
+progress_bar 3 5
 
-# Instalação do Nginx
+# Instalando o Nginx
 if [ "$INSTALL_NGINX" = "True" ]; then
   echo -e "\n---- Instalando Nginx ----"
   apt install nginx -y
@@ -103,59 +111,18 @@ EOF
 
   systemctl reload nginx
   echo "Nginx instalado e configurado."
+  progress_bar 4 5
 fi
 
 # Instalação do PHP
 if [ "$INSTALL_PHP" = "True" ]; then
   echo -e "\n---- Instalando PHP ----"
   apt install -y php${PHP_VERSION}-fpm php${PHP_VERSION}-mysql php${PHP_VERSION}-mbstring php${PHP_VERSION}-gd php${PHP_VERSION}-curl php${PHP_VERSION}-zip php${PHP_VERSION}-xml
-fi
-
-# Instalação do MariaDB
-if [ "$INSTALL_MYSQL" = "True" ]; then
-  echo -e "\n---- Instalando MariaDB ----"
-  apt install -y mariadb-server
-  systemctl enable mariadb
-  systemctl start mariadb
-
-  # Configurando senha do root
-  mysql -u root <<MYSQL_SCRIPT
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD_SET}';
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
-  echo "MariaDB instalado e configurado."
-fi
-
-# Instalação do PhpMyAdmin
-if [ "$INSTALL_PHPMYADMIN" = "True" ]; then
-  echo -e "\n---- Instalando PhpMyAdmin ----"
-  apt install -y phpmyadmin
-  ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
-  systemctl restart nginx
-fi
-
-# Habilitação de SSL com Certbot
-if [ "$INSTALL_NGINX" = "True" ] && [ "$ENABLE_SSL" = "True" ]; then
-  echo -e "\n---- Instalando Certbot e configurando SSL ----"
-  apt install -y certbot python3-certbot-nginx
-  certbot --nginx -d "$WEBSITE_NAME" --noninteractive --agree-tos --email "$ADMIN_EMAIL" --redirect
-  systemctl reload nginx
-fi
-
-# Criando banco de dados
-if [ "$CREATE_DATABASE" = "True" ] && [ "$INSTALL_MYSQL" = "True" ]; then
-  echo -e "\n---- Criando Banco de Dados ----"
-  mysql -u root -p"${MYSQL_PASSWORD_SET}" <<MYSQL_SCRIPT
-CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME};
-CREATE USER '${DATABASE_NAME}_user'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD_SET}';
-GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${DATABASE_NAME}_user'@'localhost';
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
-  echo "Banco de dados criado com sucesso."
+  progress_bar 5 5
 fi
 
 # Finalização
-echo "-----------------------------------------------------------"
+echo -e "\n-----------------------------------------------------------"
 echo "Instalação concluída com sucesso!"
 if [ "$INSTALL_NGINX" = "True" ]; then
   echo "Website disponível em: http://$WEBSITE_NAME"
